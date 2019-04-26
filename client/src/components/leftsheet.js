d254881ad0aeedb910e06027a3177414f5af791b
Max Wilets <mwilets@gmail.com>
2019-04-25 23:07:42 -0600

    left form

--- client/src/components/leftsheet.js
+++ client/src/components/leftsheet.js
@@ -11,8 +11,49 @@ class Leftsheet extends Component {
     return (
       <div>
         <div className="row">
-            <form>
-                
+            <form id="statsForm">
+                <label htmlFor="strength">STRENGTH</label>
+                    <input
+                        type="text"
+                        name="strength"
+                        value={this.state.strength}
+                        onChange={this.handleChange}
+                        />
+                    <label htmlFor="dex">DEXTERITY</label>
+                    <input
+                        type="text"
+                        name="dex"
+                        value={this.state.dex}
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="const">CONSTITUTION</label>
+                    <input
+                        type="text"
+                        name="const"
+                        value={this.state.const}
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="int">INTELLIGENCE</label>
+                    <input
+                        type="text"
+                        name="int"
+                        value={this.state.int}
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="wis">WISDOM</label>
+                    <input
+                        type="text"
+                        name="wis"
+                        value={this.state.wis}
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="char">CHARISMA</label>
+                    <input
+                        type="text"
+                        name="char"
+                        value={this.state.char}
+                        onChange={this.handleChange}
+                    />
             </form>
            <div className="col-3">
               <form>
