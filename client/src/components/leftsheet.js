7abc82ee1cafb292552d83157e918e9f9cd756ff
Max Wilets <mwilets@gmail.com>
2019-04-25 21:38:11 -0600

    front end form

---
+++ client/src/components/leftsheet.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Leftsheet extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            
+        }
+    }
+  render() {
+    return (
+      <div>
+        <div className="row">
+            <form>
+                
+            </form>
+           <div className="col-3">
+              <form>
+                  
+              </form>
+           </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Leftsheet;
