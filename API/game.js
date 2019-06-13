ffa26f3856c88ba16bf7750e1e6a50c1b6efe9e3
Max Wilets <mwilets@gmail.com>
2019-06-12 19:36:17 -0600

    router stuff

---
+++ API/game.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Sheet = require("../DB/character/sheet");
+
+router.get("/gamedata", (req, res)=>{
+
+});
+router.get("/gamedata", (req, res)=>{
+    
+});
+router.get("/gamedata", (req, res)=>{
+    
+});
+
+router.get("/gamedata", (req, res)=>{
+    
+});
+
