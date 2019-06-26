04838fbf295a6b1b40b818ebf1fe74606705ffaa
Max Wilets <mwilets@gmail.com>
2019-06-26 17:46:15 -0600

    new form

--- client/src/components/leftsheet.js
+++ client/src/components/leftsheet.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
+
 class Leftsheet extends Component {
     constructor(props){
         super(props)
         this.state={
-            
+            strength:"",
+            dex:"",
+            const:"",
+            int: "",
+            char: ""
         }
     }
   render() {
     return (
-      <div>
+      <div className="container">
         <div className="row">
+            <div className="col-3">
             <form id="statsForm">
                 <label htmlFor="strength">STRENGTH</label>
                     <input
@@ -61,10 +67,111 @@ class Leftsheet extends Component {
                         onChange={this.handleChange}
                     />
             </form>
+              
+           </div>
            <div className="col-3">
-              <form>
-                  
-              </form>
+           <form id="statsForm">
+                <label htmlFor="strength">STRENGTH</label>
+                    <input
+                        type="text"
+                        name="strength"
+                        value={this.state.strength}
+                        size="2"
+                        onChange={this.handleChange}
+                        />
+                    <label htmlFor="dex">DEXTERITY</label>
+                    <input
+                        type="text"
+                        name="dex"
+                        value={this.state.dex}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="const">CONSTITUTION</label>
+                    <input
+                        type="text"
+                        name="const"
+                        value={this.state.const}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="int">INTELLIGENCE</label>
+                    <input
+                        type="text"
+                        name="int"
+                        value={this.state.int}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="wis">WISDOM</label>
+                    <input
+                        type="text"
+                        name="wis"
+                        value={this.state.wis}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="char">CHARISMA</label>
+                    <input
+                        type="text"
+                        name="char"
+                        value={this.state.char}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+            </form>
+           </div>
+           <div className="col-3">
+           <form id="statsForm">
+                <label htmlFor="strength">STRENGTH</label>
+                    <input
+                        type="text"
+                        name="strength"
+                        value={this.state.strength}
+                        size="2"
+                        onChange={this.handleChange}
+                        />
+                    <label htmlFor="dex">DEXTERITY</label>
+                    <input
+                        type="text"
+                        name="dex"
+                        value={this.state.dex}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="const">CONSTITUTION</label>
+                    <input
+                        type="text"
+                        name="const"
+                        value={this.state.const}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="int">INTELLIGENCE</label>
+                    <input
+                        type="text"
+                        name="int"
+                        value={this.state.int}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="wis">WISDOM</label>
+                    <input
+                        type="text"
+                        name="wis"
+                        value={this.state.wis}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="char">CHARISMA</label>
+                    <input
+                        type="text"
+                        name="char"
+                        value={this.state.char}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+            </form>
            </div>
         </div>
       </div>
---
+++ client/src/components/newcharacter.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Leftsheet from './leftsheet';
+
+export default class NewCharacter extends Component {
+    render() {
+        return (
+            <div>
+                <h3>New Charactersheet</h3>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            <Leftsheet/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
