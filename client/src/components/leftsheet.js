52ca6fbeb417b79b2cfe42d349c9383c33a6d4fa
Max Wilets <mwilets@gmail.com>
2019-06-29 01:32:47 -0600

    changes

--- client/src/components/leftsheet.js
+++ client/src/components/leftsheet.js
@@ -25,39 +25,39 @@ class Leftsheet extends Component {
     return (
       <Container>
         <Row>
-            <Col lg="4" md="4" >
-            <form id="statsForm">
-                <label htmlFor="strength">STRENGTH</label>
-                    <input
-                        type="text"
-                        name="strength"
-                        value={this.state.strength}
-                        size="2"
-                        onChange={this.handleChange}
+            <Col lg="1" md="1" >
+                <form id="statsForm">
+                    <label htmlFor="strength">STRENGTH</label>
+                        <input
+                            type="text"
+                            name="strength"
+                            value={this.state.strength}
+                            size="2"
+                            onChange={this.handleChange}
                         />
-                    <label htmlFor="dex">DEXTERITY</label>
-                    <input
-                        type="text"
-                        name="dex"
-                        value={this.state.dex}
-                        size="2"
-                        onChange={this.handleChange}
-                    />
-                    <label htmlFor="const">CONSTITUTION</label>
-                    <input
-                        type="text"
-                        name="const"
-                        value={this.state.const}
-                        size="2"
-                        onChange={this.handleChange}
-                    />
-                    <label htmlFor="int">INTELLIGENCE</label>
-                    <input
-                        type="text"
-                        name="int"
-                        value={this.state.int}
-                        size="2"
-                        onChange={this.handleChange}
+                        <label htmlFor="dex">DEXTERITY</label>
+                        <input
+                            type="text"
+                            name="dex"
+                            value={this.state.dex}
+                            size="2"
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="const">CONSTITUTION</label>
+                        <input
+                            type="text"
+                            name="const"
+                            value={this.state.const}
+                            size="2"
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="int">INTELLIGENCE</label>
+                        <input
+                            type="text"
+                            name="int"
+                            value={this.state.int}
+                            size="2"
+                            onChange={this.handleChange}
                     />
                     <label htmlFor="wis">WISDOM</label>
                     <input
@@ -78,7 +78,7 @@ class Leftsheet extends Component {
             </form>
               
            </Col>
-           <Col lg={4} md={4}>
+           <Col lg={2} md={4}>
            <form id="statsForm">
                 <label htmlFor="strength">STRENGTH</label>
                     <input
@@ -132,16 +132,12 @@ class Leftsheet extends Component {
            </Col>
            <Col md={4} lg={4}>
               <p>
-                  fuck
+                  
               </p>
            </Col>
            
         </Row>
-        <Row>
-        <Col lg="4">First, but unordered</Col>
-        <Col lg="4">Second, but last</Col>
-        <Col lg="4">Third, but second</Col>
-        </Row>
+        
       </Container>
     ) 
   }
