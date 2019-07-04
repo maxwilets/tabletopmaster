82cb9d64e8f301ed5b827c3bdb4d57c5d75fca05
Max Wilets <mwilets@gmail.com>
2019-07-03 18:18:54 -0600

    meow

--- client/src/components/leftsheet.js
+++ client/src/components/leftsheet.js
@@ -93,52 +93,78 @@ class Leftsheet extends Component {
            </Col>
            <Col lg={2} md={4}>
            <form id="skills">
-                <label htmlFor="strength">STRENGTH</label>
+                <label htmlFor="acrobatics">Acrobatics</label>
                     <input
                         type="checkbox"
-                        name="Acrobatics"
+                        name="acrobatics"
                         onChange={this.handleBox}
                         />
-                    <label htmlFor="dex">DEXTERITY</label>
+                    <label htmlFor="animal">Animal Handling</label>
                     <input
-                        type="text"
-                        name="dex"
-                        value={this.state.dex}
-                        size="2"
+                        type="checkbox"
+                        name="animal"
                         onChange={this.handleChange}
                     />
-                    <label htmlFor="const">CONSTITUTION</label>
+                    <label htmlFor="arcana">Arcana</label>
                     <input
-                        type="text"
-                        name="const"
-                        value={this.state.const}
-                        size="2"
+                        type="checkbox"
+                        name="arcana"
                         onChange={this.handleChange}
                     />
-                    <label htmlFor="int">INTELLIGENCE</label>
+                    <label htmlFor="athletic">Athletics</label>
                     <input
-                        type="text"
-                        name="int"
-                        value={this.state.int}
-                        size="2"
+                        type="checkbox"
+                        name="athletic"
                         onChange={this.handleChange}
                     />
-                    <label htmlFor="wis">WISDOM</label>
+                    <label htmlFor="decpt">Deception</label>
                     <input
-                        type="text"
-                        name="wis"
-                        value={this.state.wis}
-                        size="2"
+                        type="checkbox"
+                        name="decept"
                         onChange={this.handleChange}
                     />
-                    <label htmlFor="char">CHARISMA</label>
+                    <label htmlFor="hist">History</label>
                     <input
-                        type="text"
-                        name="char"
-                        value={this.state.char}
-                        size="2"
+                        type="checkbox"
+                        name="hist"
                         onChange={this.handleChange}
                     />
+                    <label htmlFor="insight">Insight</label>
+                    <input
+                        type="checkbox"
+                        name="instight"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="intimid">Intimidation</label>
+                    <input
+                        type="checkbox"
+                        name="intimid"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="invest">Investigation</label>
+                    <input
+                        type="checkbox"
+                        name="invest"
+                        onChange={this.handleChange}
+                    /> 
+                    <label htmlFor="med">Medicine</label>
+                    <input
+                        type="checkbox"
+                        name="med"
+                        onChange={this.handleChange}
+                    /> 
+                    <label htmlFor="nature">Nature</label>
+                    <input
+                        type="checkbox"
+                        name="nature"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="percept">Perception</label>
+                    <input
+                        type="checkbox"
+                        name="percept"
+                        onChange={this.handleChange}
+                    />                                    
             </form>
            </Col>
            <Col md={4} lg={4}>
