f1b9ec8a8d240f96397f903d1d61b178b04eaeaa
Max Wilets <mwilets@gmail.com>
2019-07-10 14:05:20 -0600

    react test

--- client/src/components/testComp/leftTest.js
+++ client/src/components/testComp/leftTest.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 export default class LeftTest extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            strength:"",
+            dex:"",
+            const:"",
+            int: "",
+            char: ""
+        }
+        this.handleChange= this.handleChange.bind(this);
+    };
+    handleChange(event){
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+      }
     render() {
         return (
             <div>
