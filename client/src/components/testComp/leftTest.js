fb5c914721f26a4a53cf8e34373b42dd900ebca2
Max Wilets <mwilets@gmail.com>
2019-07-10 14:02:47 -0600

    reacdttest

---
+++ client/src/components/testComp/leftTest.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+export default class LeftTest extends Component {
+    render() {
+        return (
+            <div>
+                <form id="statsForm">
+                    <label htmlFor="strength">STRENGTH</label>
+                        <input
+                            type="text"
+                            name="strength"
+                            value={this.state.strength}
+                            size="2"
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="dex">DEXTERITY</label>
+                        <input
+                            type="text"
+                            name="dex"
+                            value={this.state.dex}
+                            size="2"
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="const">CONSTITUTION</label>
+                        <input
+                            type="text"
+                            name="const"
+                            value={this.state.const}
+                            size="2"
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="int">INTELLIGENCE</label>
+                        <input
+                            type="text"
+                            name="int"
+                            value={this.state.int}
+                            size="2"
+                            onChange={this.handleChange}
+                    />
+                    <label htmlFor="wis">WISDOM</label>
+                    <input
+                        type="text"
+                        name="wis"
+                        value={this.state.wis}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="char">CHARISMA</label>
+                    <input
+                        type="text"
+                        name="char"
+                        value={this.state.char}
+                        size="2"
+                        onChange={this.handleChange}
+                    />
+            </form>
+            </div>
+        )
+    }
+}
